7af7e950c48da55c1ff8aef04da379283cb4e5d5
Léonard Roussel <leonard.roussel@gmail.com>
2020-07-19 16:11:27 -0400

    published! v1
    added CV

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -10,6 +10,7 @@
   function yourThings() {
   	var things = [ 	"idea",
   					"drawing", 
+            "artist friend",
   					"video game", 
   					"theater play", 
   					"movie script",
@@ -19,16 +20,14 @@
   					"piece",
   					"boyfriend",
   					"girlfriend",
-  					"passion",
   					"crazy thought",
-  					"dog",
-  					"cat",
   					"book",
   					"story",
   					"favorite thing",
   					"artwork",
   					"big thing",
   					"job",
+            "diet",
   					"video",
   					"movie",
   					"website",
@@ -37,7 +36,7 @@
   					"painting",
   					"house",
   					"studio",
-  					"musical instruments",
+  					"musical instrument",
   					"dance performance",
   					"light show",
   					"conference",
@@ -49,37 +48,30 @@
   					"building",
   					"space",
   					"soundtrack",
-  					"backyard",
-  					"pool",
   					"design",
-  					"drawing",
   					"bicycle",
-  					"diet",
   					"discovery",
+            "breakthrough",
   					"plans",
-  					"project",
   					"spiritual practice",
   					"collaboration",
   					"festival",
   					"masterplan",
   					"podcast" ];
-  	// $(".your-thing").html(things[0]);
-  	
-  	// METHOD 1
-  	// setTimeout(() => {  $(".your-thing").html(things[1]); }, 1000);
-  	// setTimeout(() => {  $(".your-thing").html(things[2]); }, 2000);
 
-  	// METHOD 2
-  	sleep(5000).then(() => { shuffle(things); });
+  	sleep(5000).then(() => { shuffle(); });
 
-  	async function shuffle(array) {  		
+  	async function shuffle() {  		
   		while(true){
-  			array.sort(() => Math.random() - 0.5);
+  			things.sort(() => Math.random() - 0.5);
 
-	  		for (var i = array.length - 1; i >= 0; i--) {
+	  		for (var i = Math.floor(things.length/2) - 1; i >= 0; i--) {
 	  			$(".your-thing").html(things[i]);
-	  			await sleep(1000);
+	  			await sleep(300);
 	  		}
+
+        $(".your-thing").html("project");
+        await sleep(5000);
 	  	}
   	}
   }
